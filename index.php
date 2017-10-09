f902efaf1472c92c5c45275e08fe5c6391a8eef8
dylan326 <30761594+dylan326@users.noreply.github.com>
2017-10-09 15:04:25 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html><head>
+        <link rel="stylesheet" type="text/css" href="style.css"> 
+    </head>
+    <body>
+        <h3><a href="database.php">Explore Database</a></h3>
+        <p><a href="form_account.php">Create New Account</a></p>
+        <p><a href="form_question.php">Create New Question</a></p>
+        <p><a href="form_8track.php">Create New 8-Track</a></p>
+        <p><a href="form_topic.php">Add Topic</a></p>
+        <p><a href="form_team.php">Add Team</a></p>
+
+    </body>
+</html>
